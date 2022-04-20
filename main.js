1ab4e51ee9fa6f686c085cdeb78dcd053a8bf4f3
Thanapolseekhot <52207987+Thanapolseekhot@users.noreply.github.com>
2022-04-20 10:14:43 +0700

    update

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ const app = Vue.createApp({
             inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg',quantity:50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg',quantity:0 },
             ]
         }
     },
@@ -17,8 +17,13 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
+        },
+        computed:{
+            title(){
+                return this.brand+' '+this.product
+            }
         }
     }
 })
